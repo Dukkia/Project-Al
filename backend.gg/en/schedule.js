b14083f4f5f77888362232e4fcaef403463b9c6a
Dukki <lafci02@gmail.com>
2024-05-24 15:39:07 +0900

    v1.6
    EPL 경기 일정 Backend 코드 추가

---
+++ backend.gg/en/schedule.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const axios = require('axios');
+const moment = require('moment-timezone');
+const NodeCache = require('node-cache');
+const app = express();
+const cors = require('cors');
+
+const port = 4401;
+
+const allowedOrigins = ['http://localhost:5173'];
+
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ['GET']
+}));
+
+const cache = new NodeCache({ stdTTL: 2592000 }); // 캐시 TTL(유효 시간)을 2592000초(30일)로 설정
+
+app.get('/', async (req, res) => {
+    try {
+        // 캐시에 데이터가 있는지 확인
+        const cachedData = cache.get('formattedData');
+
+        if (cachedData) {
+            console.log('Data found in cache');
+            res.json(cachedData); // 캐시에서 데이터를 클라이언트에 전송
+            return;
+        }
+
+        // 캐시에 데이터가 없는 경우, 외부 API에서 데이터를 가져와서 포맷팅
+        const response = await axios.get('https://api.performfeeds.com/soccerdata/match/1wajy57wfq6wo1qnta55rgx3an?comp=2kwbbcootiqqgmrzs6o5inle5&_pgSz=1000&_rt=b&_fmt=json');
+        const data = response.data;
+
+        const matches = data.match;
+
+        const formattedData = matches.map(match => {
+            const dateTime = match.matchInfo.date + 'T' + match.matchInfo.time;
+            const koreaTime = moment.utc(dateTime.replace('Z', '')).tz('Asia/Seoul');
+            return {
+                Date: koreaTime.format('YYYY-MM-DD'),
+                Time: koreaTime.format('HH:mm:ss'),
+                Team: [
+                    { position: 'home', name: match.matchInfo.contestant[0].name },
+                    { position: 'away', name: match.matchInfo.contestant[1].name }
+                ],
+                Result: match.liveData.matchDetails.scores ? match.liveData.matchDetails.scores.total : "예정",
+                Round: match.matchInfo.week,
+                Place: match.matchInfo.venue.shortName
+            };
+        });
+
+        // 포맷팅된 데이터를 캐시에 저장
+        cache.set('formattedData', formattedData);
+
+        // 클라이언트에 JSON 형태로 데이터를 보냄
+        res.json(formattedData);
+    } catch (error) {
+        console.error('External API call failed:', error);
+        res.status(500).json({ error: 'API 호출에 실패했습니다.' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`서버가 http://localhost:${port}/ 에서 실행 중입니다.`);
+});
---
+++ backend.gg/ko/cache.js
@@ -0,0 +1,49 @@
+// cache.js
+const axios = require('axios');
+const NodeCache = require('node-cache');
+
+const translationCache = new NodeCache({ stdTTL: 2592000 });
+
+const translateText = async (text) => {
+  const cachedTranslation = translationCache.get(text);
+  if (cachedTranslation) {
+    console.log(`Translation found in cache for "${text}"`);
+    return cachedTranslation;
+  }
+  try {
+    const response = await axios.post(
+      'https://naveropenapi.apigw.ntruss.com/nmt/v1/translation',
+      {
+        source: 'en',
+        target: 'ko',
+        text: text,
+        glossaryKey: 'c9e14462-1549-4216-ae38-ec65a0997869'
+      },
+      {
+        headers: {
+          'X-NCP-APIGW-API-KEY-ID': 'tl35fbwl8i',
+          'X-NCP-APIGW-API-KEY': 'R9VMF466TYFA2enPzk7eOhQuRx1XJTJYveUSnhLv',
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    const translatedText = response.data.message.result.translatedText;
+    translationCache.set(text, translatedText, (err, success) => {
+      if (!err && success) {
+        console.log(`Cached translation for "${text}"`);
+      } else {
+        console.error(`Failed to cache translation for "${text}"`);
+      }
+    });
+    console.log(`Translation retrieved and cached for "${text}"`);
+    return translatedText;
+  } catch (error) {
+    console.error('Error translating text:', error);
+    return text;
+  }
+};
+
+module.exports = {
+  translateText,
+  translationCache
+};
